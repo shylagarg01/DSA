54aaf3a647ce286ed5f5a7860b6050d45def8a23
shylagarg01 <shylagarg28@gmail.com>
2026-09-10 17:50:26 +0530

    Add middle node finder for linked list
    Implement function to find and print the middle node of a linked list.

---
+++ FindMiddleNodeInLinkedList.java
@@ -0,0 +1,51 @@
+class Main {
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    // static void display(Node head){
+    //     if(head == null){
+    //         return;
+    //     }
+    //     System.out.println(head.data + " ");
+    //     display(head.next);
+    // }
+    
+
+       
+    static void middleNode(Node head){
+        Node slow = head;
+        Node fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        System.out.println(slow.data);
+    }
+
+   
+    public static void main(String[] args) {
+        // System.out.println("Start small. Ship something.");
+        Node a = new Node(100);
+        Node b = new Node(20);
+        Node c = new Node(30);
+        Node d = new Node(40);
+        Node e = new Node(50);
+        Node f = new Node(60);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = f;
+        
+
+        middleNode(a);
+
+        
+    }
+}
+
