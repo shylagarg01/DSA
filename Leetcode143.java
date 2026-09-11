ccf174395c42281693faf422e51934f38fd08365
shylagarg01 <shylagarg28@gmail.com>
2026-09-11 05:46:14 +0530

    Implement reorderList method for linked list

---
+++ Leetcode143.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void reorderList(ListNode head) {
+        if(head==null || head.next==null){
+            return;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode curr = slow.next;
+        slow.next = null;
+        ListNode prev = null;
+        while(curr!=null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        ListNode first = head;
+        ListNode second = prev;
+        while(first!=null && second!=null){
+            ListNode firstNext = first.next;
+            ListNode secondNext = second.next;
+            first.next = second;
+            second.next = firstNext;
+            first = firstNext;
+            second = secondNext;
+        }
+    }
+}
